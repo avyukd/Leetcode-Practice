61aa000b03c5af9922fa344bff00c1fae121c8dc
avyukd <avyukd@gmail.com>
2021-08-25 18:06:31 -0400

    added two sum explanation

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        /*
+            Explanation: We need to find two numbers in the array that sum to target.
+            We can use a hashmap to store their value and indices.
+            The value stored in the hashmap is target - current value.
+            If the current value is in the hashmap, that means its complement is in the array.
+            Thus, we have found the two numbers. 
+        */
         vector<int> solution(2);
         unordered_map<int, int> m; 
         for(int i = 0 ; i < nums.size(); i++){
