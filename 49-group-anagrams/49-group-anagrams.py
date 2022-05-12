ee15963a2c229f66c9bddc65aa6797cc5321b788
avyukd <avyukd@gmail.com>
2022-05-12 12:56:06 -0400

    Time: 169 ms (29.46%), Space: 18.9 MB (32.52%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freq_temp = [0] * 26
+        uq_freqs = {}
+        for s in strs:
+            s_freq = freq_temp.copy()
+            for c in s:
+                s_freq[ord(c) - 97] += 1
+            s_freq = tuple(s_freq)
+            if s_freq in uq_freqs:
+                uq_freqs[s_freq].append(s)
+            else:
+                uq_freqs[s_freq] = [s]
+        return list(uq_freqs.values())
+        
