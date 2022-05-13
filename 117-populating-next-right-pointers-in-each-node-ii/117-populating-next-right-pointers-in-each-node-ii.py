a9843c7152e9ae1cd7eee31783639587334beeed
avyukd <avyukd@gmail.com>
2022-05-13 11:36:19 -0400

    Time: 56 ms (71.68%), Space: 15.5 MB (11.90%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -9,28 +9,53 @@ class Node:
 """
 
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
+# leetcode recommended solution: add connections as you take things out of the queue
+    def connect(self, root: Optional['Node']) -> Optional['Node']:
         if root is None:
             return root
 
-        nodes = []
-        queue = []
-        queue.append(root)
-        while len(queue) != 0:
-            level = queue.copy()
-            nodes.append(level)
-            queue = []
-            for node in level:
-                if node.left is not None:
-                    queue.append(node.left)
-                if node.right is not None:
-                    queue.append(node.right)
+        queue = deque([root])
+
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                
+                nxt = queue.popleft()
+
+                if i < size - 1:
+                    nxt.next = queue[0]
+                
+                # Add the children, if any, to the back of
+                # the queue
+                if nxt.left:
+                    queue.append(nxt.left)
+                if nxt.right:
+                    queue.append(nxt.right)
         
-        for level in nodes:
-            if len(level) > 1:
-                for i in range(len(level) - 1):
-                    level[i].next = level[i+1]
-        
+        # Since the tree has now been modified, return the root node
         return root
+#     def connect(self, root: 'Node') -> 'Node':
+#         if root is None:
+#             return root
+
+#         levels = []
+#         queue = []
+#         queue.append(root)
+#         while len(queue) != 0:
+#             level = queue.copy()
+#             levels.append(level)
+#             queue = []
+#             for node in level:
+#                 if node.left is not None:
+#                     queue.append(node.left)
+#                 if node.right is not None:
+#                     queue.append(node.right)
+        
+#         for level in levels:
+#             if len(level) > 1:
+#                 for i in range(len(level) - 1):
+#                     level[i].next = level[i+1]
+        
+#         return root
             
         
