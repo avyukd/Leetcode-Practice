8776f9b9415ece4b7bda6e235848640450857567
avyukd <avyukd@gmail.com>
2022-05-14 12:57:27 -0400

    Time: 78 ms (5.32%), Space: 13.8 MB (75.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,4 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # (n - 1 + m - 1)!/(n - 1)!(m - 1)!
         return math.factorial(n - 1 + m - 1) // (math.factorial(n-1) * math.factorial(m-1))
         
