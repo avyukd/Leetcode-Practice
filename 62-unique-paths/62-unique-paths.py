ca7a411bd9ce28ad70784ae4d63d6424743475ec
avyukd <avyukd@gmail.com>
2022-05-14 13:04:44 -0400

    Time: 35 ms (77.79%), Space: 13.8 MB (75.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,3 +1,20 @@
 class Solution:
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #   return math.comb(m + n - 2, n - 1)
+    # DP solution
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m + n - 2, n - 1)
+        memo = [[0]*(n+1) for _ in range(m+1)]
+        memo[1][1] = 1
+        
+        print(memo)
+        
+        for j in range(2,n+1):
+            memo[1][j] = 1
+        for i in range(2,m+1):
+            memo[i][1] = 1
+        
+        for i in range(2, m+1):
+            for j in range(2, n+1):
+                memo[i][j] = memo[i-1][j] + memo[i][j-1]
+        
+        return memo[m][n]
