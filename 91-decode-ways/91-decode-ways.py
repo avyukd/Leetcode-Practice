4fec366504b2d5b05908976211a85f1a60349e9b
avyukd <avyukd@gmail.com>
2022-05-15 12:46:51 -0400

    Time: 43 ms (55.73%), Space: 14.5 MB (7.55%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numDecodings(self, s: str, memo={}) -> int:
+        if len(s) == 0:
+            return 1
+        elif len(s) == 1:
+            if int(s) == 0:
+                return 0
+            else:
+                return 1
+        
+        tens = int(s[-2])
+        ones = int(s[-1])
+        full = tens*10 + ones
+        
+        if s[:-2] in memo:
+            lastTwo = memo[s[:-2]]
+        else:
+            lastTwo = self.numDecodings(s[:-2])
+            memo[s[:-2]] = lastTwo
+            
+        if s[:-1] in memo:
+            lastOne = memo[s[:-1]]
+        else:
+            lastOne = self.numDecodings(s[:-1])
+            memo[s[:-1]] = lastOne
+        
+        if full <= 26 and full >= 10:
+            if ones != 0:
+                return lastTwo + lastOne
+            else:
+                return lastTwo
+        else:
+            if ones != 0:
+                return lastOne
+            else:
+                return 0
+        
