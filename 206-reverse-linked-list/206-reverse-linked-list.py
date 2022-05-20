3f1ceea20d972bfee34014bfc7e7ad1fb5c455d8
avyukd <avyukd@gmail.com>
2022-05-19 20:18:59 -0400

    Time: 71 ms (11.50%), Space: 15.4 MB (94.24%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,27 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     if head is None:
+    #         return None
+    #     elif head.next is None:
+    #         return head
+    #     rvrsd = self.reverseList(head.next)
+    #     tmp = rvrsd
+    #     while tmp.next is not None:
+    #         tmp = tmp.next
+    #     tmp.next = head
+    #     head.next = None
+    #     return rvrsd
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
-        elif head.next is None:
-            return head
-        rvrsd = self.reverseList(head.next)
-        tmp = rvrsd
-        while tmp.next is not None:
-            tmp = tmp.next
-        tmp.next = head
-        head.next = None
-        return rvrsd
-#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         back, front = None, head
-#         while front.next is not None:
-#             tmp = front.next
-#             front.next = back
-#             front = tmp
-#             if back is None:
-#                 back = head
-#             else:
-#                 back = back.next
-#         return front
+        back, front = None, head
+        while front is not None:
+            tmp = front.next
+            front.next = back
+            back = front
+            front = tmp
+        return back
             
