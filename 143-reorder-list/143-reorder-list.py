d350ae281bf1c27fba6335d80cbb81cb60e09e17
avyukd <avyukd@gmail.com>
2022-05-21 20:31:13 -0400

    Time: 125 ms (44.13%), Space: 24.2 MB (30.51%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # 1 get length of list
+        tmp = head
+        size = 0
+        while tmp is not None:
+            tmp = tmp.next
+            size += 1
+        
+        # 2 put second half into stack
+        stack = deque()
+        tmp = head
+        i = 0
+        while i < (size / 2):
+            tmp = tmp.next
+            i += 1
+        while tmp is not None:
+            stack.append(tmp)
+            tmp = tmp.next
+        # 3 reorder the list
+        tmphead = head
+        while stack:
+            nxt = tmphead.next
+            toins = stack.pop()
+            tmphead.next = toins
+            toins.next = nxt
+            print(tmphead.val)
+            print(toins.val)
+            print(nxt.val)
+            tmphead = nxt
+        tmphead.next = None
+        return head
