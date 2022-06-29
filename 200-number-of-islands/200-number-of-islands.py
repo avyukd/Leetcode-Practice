8b90f25f3438572f1402d5cd35a2da3d2ce32179
avyukd <avyukd@gmail.com>
2022-06-28 20:49:50 -0400

    Time: 429 ms (59.55%), Space: 21.9 MB (25.08%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
@@ -8,13 +10,23 @@ class Solution:
         
         directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         
-        def dfs(i, j):
-            if (i, j) not in visited and grid[i][j] == "1":
-                visited.add((i, j))
-                for direction in directions:
-                    next_i, next_j = i + direction[0], j + direction[1]
-                    if 0 <= next_i < m and 0 <= next_j < n:
-                        dfs(next_i, next_j)
+        def dfs(og_i, og_j):
+            stack = deque([(og_i, og_j)])
+            while stack:
+                (i, j) = stack.pop()
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    visited.add((i, j))
+                    for direction in directions:
+                        next_i, next_j = i + direction[0], j + direction[1]
+                        if 0 <= next_i < m and 0 <= next_j < n:
+                            stack.append((next_i, next_j))
+            
+#             if (i, j) not in visited and grid[i][j] == "1":
+#                 visited.add((i, j))
+#                 for direction in directions:
+#                     next_i, next_j = i + direction[0], j + direction[1]
+#                     if 0 <= next_i < m and 0 <= next_j < n:
+#                         dfs(next_i, next_j)
                 
         
         for i in range(m):
