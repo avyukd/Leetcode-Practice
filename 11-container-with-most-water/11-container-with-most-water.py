c620cf112ca16fa9293659101cac82eaf1475007
avyukd <avyukd@gmail.com>
2022-07-02 21:56:55 -0400

    Time: 1434 ms (12.66%), Space: 27.2 MB (94.05%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,20 +1,17 @@
 class Solution:
-    # two pointer
     def maxArea(self, height: List[int]) -> int:
-        maxarea = 0
         i, j = 0, len(height) - 1
+        maxVal = 0
         while i < j:
-            maxarea = max(min(height[i],height[j])*(j-i), maxarea)
+            print(i, j)
+            maxVal = max(maxVal, (j - i) * min(height[i], height[j]))
+            
             if height[i] < height[j]:
-                i+=1
+                i += 1
+            elif height[i] > height[j]:
+                j -= 1
             else:
-                j-=1
-        return maxarea
-    # brute force
-    # def maxArea(self, height: List[int]) -> int:
-    #     maxarea = 0
-    #     for i in range(len(height)):
-    #         for j in range(len(height)):
-    #             if i < j:
-    #                 maxarea = max(maxarea,min(height[i],height[j])*(j-i))
-    #     return maxarea
+                i += 1
+                j -= 1
+        
+        return maxVal
