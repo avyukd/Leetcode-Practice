4e759f737a3982a542748469f29fc7237997b9ba
avyukd <avyukd@gmail.com>
2022-07-04 17:29:39 -0400

    Time: 144 ms (34.55%), Space: 14.3 MB (98.77%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,28 +1,19 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if nums == []:
+        if len(nums) == 0:
             return 0
-        elif len(nums) == 1:
-            return nums[0]
-        
-        negs = []
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                return max(self.maxProduct(nums[:i]), self.maxProduct(nums[i+1:]), 0)
-            if nums[i] < 0:
-                negs.append(i)
-            
-        if len(negs) % 2 == 0:
-            prod = 1
-            for num in nums:
-                prod *= num
-            return prod
-        else:
-            maxProd = nums[0]
-            for i in negs:
-                left = self.maxProduct(nums[:i])
-                left = max(left*nums[i],left)
-                right = self.maxProduct(nums[i+1:])
-                right = max(right*nums[i],right)
-                maxProd = max([maxProd, left, right])
-            return maxProd
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(max_so_far, result)
+
+        return result
