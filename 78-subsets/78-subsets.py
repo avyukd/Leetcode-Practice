652221384b25f8671e577c616fa1a9f6a8a136f5
avyukd <avyukd@gmail.com>
2022-07-06 21:44:11 -0400

    Time: 55 ms (42.06%), Space: 14.1 MB (34.00%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,10 +1,27 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        # cascading - for each num in nums, append to all existing in output
-        output = [[]]
-        for num in nums:
-            output += [curr + [num] for curr in output]
-        return output
+    def subsets(self, nums):
+        n = len(nums)
+        res, subset = [], []
+        def dfs(i):
+            if i == n:
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i+1)
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
+#     def subsets(self, nums):
+#         for i in range(2**len(nums), 2**(len(nums)+1)):
+#             print(bin(i))
+    
+#     # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     # cascading - for each num in nums, append to all existing in output
+    #     output = [[]]
+    #     for num in nums:
+    #         output += [curr + [num] for curr in output]
+    #     return output
 # first atetmpt but slow and inelegant
 #     def subsets(self, nums: List[int]) -> List[List[int]]:
 #         result = set()
