ab9ee11114107ce61bb4ea529767639831ae73f8
avyukd <avyukd@gmail.com>
2022-07-06 21:05:04 -0400

    Time: 51 ms (52.39%), Space: 14 MB (96.35%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,21 +1,28 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = set()
-        n = len(nums)
-        def backtrack(indices):
-            subset = []
-            for i in range(n):
-                if indices[i]:
-                    subset.append(nums[i])
+        # cascading - for each num in nums, append to all existing in output
+        output = [[]]
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        return output
+# first atetmpt but slow and inelegant
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         result = set()
+#         n = len(nums)
+#         def backtrack(indices):
+#             subset = []
+#             for i in range(n):
+#                 if indices[i]:
+#                     subset.append(nums[i])
             
-            subset = tuple(subset)
+#             subset = tuple(subset)
             
-            if subset not in result:
-                result.add(subset)
-                for i in range(n):
-                    if indices[i] != 1:
-                        copy = indices[:i] + [1] + indices[i+1:]
-                        backtrack(copy)
+#             if subset not in result:
+#                 result.add(subset)
+#                 for i in range(n):
+#                     if indices[i] != 1:
+#                         copy = indices[:i] + [1] + indices[i+1:]
+#                         backtrack(copy)
         
-        backtrack([0] * n)
-        return [list(x) for x in result]
+#         backtrack([0] * n)
+#         return [list(x) for x in result]
