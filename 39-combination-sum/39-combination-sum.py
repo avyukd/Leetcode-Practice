22ca81232eb957a3c42caa25d15194cf2f561088
avyukd <avyukd@gmail.com>
2022-07-08 17:42:19 -0400

    Time: 181 ms (21.01%), Space: 14.2 MB (26.58%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,23 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
         res = set()
         perm = []
-        def backtrack(idx):
+        def backtrack():
             if sum(perm) == target:
                 res.add(tuple(perm[:]))
                 return
             elif sum(perm) > target:
                 return
-            for i in range(idx, len(candidates)):
-                j = 1
-                if len(perm) == 0 or candidates[i] > perm[-1]:
-                    while sum(perm) + j * candidates[i] <= target:
-                        for _ in range(j):
-                            perm.append(candidates[i])
-                        backtrack(idx + 1)
-                        for _ in range(j):
-                            perm.pop()
-                        j += 1
-        backtrack(0)
+            for i in range(len(candidates)):
+                if len(perm) == 0 or candidates[i] >= perm[-1]:
+                    perm.append(candidates[i])
+                    backtrack()
+                    perm.pop()
+        backtrack()
         return list(res)
