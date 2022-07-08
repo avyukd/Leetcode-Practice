5a49c5dd1382a7f5302857284abbc81df4d43261
avyukd <avyukd@gmail.com>
2022-07-08 17:32:45 -0400

    Time: 176 ms (23.07%), Space: 14.3 MB (11.08%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,8 +2,6 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = set()
         perm = []
-        local = defaultdict(int)
-        candidateFreqs = defaultdict(set)
         def backtrack():
             if sum(perm) == target:
                 res.add(tuple(perm[:]))
@@ -13,9 +11,7 @@ class Solution:
             for i in range(len(candidates)):
                 if len(perm) == 0 or candidates[i] >= perm[-1]:
                     perm.append(candidates[i])
-                    local[candidates[i]] += 1
                     backtrack()
                     perm.pop()
-                    local[candidates[i]] -= 1
         backtrack()
         return list(res)
