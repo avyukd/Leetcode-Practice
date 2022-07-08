521a0a15ddbb3c85bc02eea50bdf3163e60cf1af
avyukd <avyukd@gmail.com>
2022-07-08 17:32:29 -0400

    Time: 203 ms (14.31%), Space: 14.2 MB (26.58%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,54 +1,21 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        results = []
-        candidates.sort()
-        def backtrack(remain, comb, start_idx):
-            if remain == 0:
-                results.append(comb.copy())
+        res = set()
+        perm = []
+        local = defaultdict(int)
+        candidateFreqs = defaultdict(set)
+        def backtrack():
+            if sum(perm) == target:
+                res.add(tuple(perm[:]))
                 return
-            elif remain < 0:
+            elif sum(perm) > target:
                 return
-            
-            for i in range(start_idx, len(candidates)):
-                comb.append(candidates[i])
-                backtrack(remain - candidates[i], comb, i)
-                comb.pop()
-        
-        backtrack(target, [], 0)
-        return results
-    
-#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-#         # key is sum, value is list of all combinations
-#         memo = {}
-        
-#         for i in range(1, target+1):
-#             memo[i] = []
-        
-#         for num in candidates:
-#             if num <= target:
-#                 memo[num].append([num])
-        
-#         for n in range(1, target + 1):
-#             for k in range(1, n // 2 + 1):
-#                 # n - k, k is combination we want to join
-#                 for comb1 in memo[n - k]:
-#                     for comb2 in memo[k]:
-#                         comb = comb1 + comb2
-#                         print(comb)
-#                         memo[n].append(comb)
-        
-#         for comb in memo[target]:
-#             freq = {}
-#             for x in comb:
-#                 if x not in freq:
-#                     freq[x] = 1
-#                 else:
-#                     freq[x] += 1
-            
-        
-#         return memo[target]
-
-        
-        
-        
-        
+            for i in range(len(candidates)):
+                if len(perm) == 0 or candidates[i] >= perm[-1]:
+                    perm.append(candidates[i])
+                    local[candidates[i]] += 1
+                    backtrack()
+                    perm.pop()
+                    local[candidates[i]] -= 1
+        backtrack()
+        return list(res)
