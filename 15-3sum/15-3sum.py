dddd1161b93503f5c16f6dc9fb6732b4bc411274
avyukd <avyukd@gmail.com>
2022-07-09 12:22:42 -0400

    Time: 4148 ms (18.58%), Space: 17 MB (94.78%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,50 +1,17 @@
-class Solution:
-    
-    # sort and two pointers
-    
+class Solution:    
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         nums.sort()
-        
-        triplets = set()
-        
-        for i in range(len(nums)):
-            num = nums[i]
-            rest = nums[0:i] + nums[i+1:]
-            start, end = 0, len(rest) - 1
-            while start < end:
-                if rest[start] + rest[end] < -num:
-                    start+=1
-                elif rest[start] + rest[end] > -num:
-                    end-=1
+        result = set()
+        for i in range(1, len(nums) - 1):
+            j, k = 0, len(nums) - 1
+            while j < i and k > i:
+                curr = nums[i] + nums[j] + nums[k] 
+                if curr > 0:
+                    k -= 1
+                elif curr < 0:
+                    j += 1
                 else:
-                    x = [rest[start],num,rest[end]]
-                    x.sort()
-                    triplets.add(tuple(x))
-                    start+=1
-                    end-=1
-        
-        return triplets
-    
-    # brute force-ish solution
-    # def threeSum(self, nums: List[int]) -> List[List[int]]:
-    #     triplets = set()
-    #     for i in range(len(nums)):
-    #         num = nums[i]
-    #         rest = nums[0:i] + nums[i+1:]
-    #         # fix number and find sum to -num in rest of list
-    #         seen = set()
-    #         dbls = []
-    #         for r in rest:
-    #             if -num - r in seen:
-    #                 dbls.append([min(r,-num -r), max(r,-num - r)])
-    #             else:
-    #                 seen.add(r)
-    #         for dbl in dbls:
-    #             if num < dbl[0]:
-    #                 triplets.add((num,dbl[0],dbl[1]))
-    #             elif num > dbl[1]:
-    #                 triplets.add((dbl[0],dbl[1],num))
-    #             else:
-    #                 triplets.add((dbl[0],num,dbl[1]))
-    #     return list(triplets)
+                    result.add((nums[j], nums[i], nums[k]))
+                    k -= 1
+                    j += 1
+        return list(result)
