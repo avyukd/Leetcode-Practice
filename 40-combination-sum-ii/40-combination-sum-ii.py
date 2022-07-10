3046e50eac8dca63cac53c9fe751229f1fa897ca
avyukd <avyukd@gmail.com>
2022-07-10 12:35:06 -0400

    Time: 190 ms (14.70%), Space: 14.1 MB (22.35%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,26 +1,52 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        results = []
-                
-        def backtrack(comb, remain, start, counter, candidates):
+    def combinationSum2(self, candidates, target):
+        res = []
+        uq_candidates = Counter(candidates)
+        keys = list(uq_candidates.keys())
+        keys.sort()
+        def backtrack(remain, comb, start):
             if remain == 0:
-                results.append(comb.copy())
+                res.append(comb[:])            
                 return
             elif remain < 0:
                 return
             
-            for num in candidates[start:]:
-                if counter[num] > 0:
-                    if len(comb) == 0 or num >= comb[-1]:
-                        counter[num] -= 1
-                        comb.append(num)
+            for i in range(start, len(keys)):
+                if uq_candidates[keys[i]] > 0:
+                    uq_candidates[keys[i]] -= 1
+                    backtrack(remain - keys[i], comb + [keys[i]], i)
+                    uq_candidates[keys[i]] += 1
+        backtrack(target, [], 0)    
+        
+        return list(res)
+    
+    
+    
+    
+    
+    
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         results = []
+                
+#         def backtrack(comb, remain, start, counter, candidates):
+#             if remain == 0:
+#                 results.append(comb.copy())
+#                 return
+#             elif remain < 0:
+#                 return
+            
+#             for num in candidates[start:]:
+#                 if counter[num] > 0:
+#                     if len(comb) == 0 or num >= comb[-1]:
+#                         counter[num] -= 1
+#                         comb.append(num)
 
-                        backtrack(comb, remain - num, start, counter, candidates)
+#                         backtrack(comb, remain - num, start, counter, candidates)
 
-                        comb.pop()
-                        counter[num] += 1
+#                         comb.pop()
+#                         counter[num] += 1
                     
-        uq_candidates = list(set(candidates))
-        uq_candidates.sort()
-        backtrack([], target, 0, Counter(candidates), uq_candidates)
-        return results
+#         uq_candidates = list(set(candidates))
+#         uq_candidates.sort()
+#         backtrack([], target, 0, Counter(candidates), uq_candidates)
+#         return results
