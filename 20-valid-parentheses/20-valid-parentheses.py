d83af6c475853475f4e6851a5c183893498598cc
avyukd <avyukd@gmail.com>
2022-07-11 19:09:32 -0400

    Time: 38 ms (78.66%), Space: 13.9 MB (70.03%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,21 +1,45 @@
 from collections import deque
 
 class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = deque()
-        op = ['(','{','[']
-        for c in s:
-            if c in op:
-                stack.append(c)
+    def isValid(self, s):
+        left = ('(', '{', '[')
+        stack = deque([])
+        for ch in s:
+            if ch in left:
+                stack.append(ch)
             else:
-                if len(stack) == 0:
-                    return False
-                old = stack[-1]
-                if (old == '(' and c == ')') or \
-                    (old == '{' and c == '}') or \
-                    (old == '[' and c == ']'):
-                    stack.pop()
+                if stack:
+                    top = stack.pop()
                 else:
                     return False
-        return len(stack) == 0
+                if ch == ')' and top != '(':
+                    return False
+                elif ch == '}' and top != '{':
+                    return False
+                elif ch == ']' and top != '[':
+                    return False
+        return not stack
+    
+    
+    
+    
+    
+    
+#     def isValid(self, s: str) -> bool:
+#         stack = deque()
+#         op = ['(','{','[']
+#         for c in s:
+#             if c in op:
+#                 stack.append(c)
+#             else:
+#                 if len(stack) == 0:
+#                     return False
+#                 old = stack[-1]
+#                 if (old == '(' and c == ')') or \
+#                     (old == '{' and c == '}') or \
+#                     (old == '[' and c == ']'):
+#                     stack.pop()
+#                 else:
+#                     return False
+#         return len(stack) == 0
                     
