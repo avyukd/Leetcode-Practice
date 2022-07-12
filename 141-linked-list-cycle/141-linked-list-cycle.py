733ff009dcbf087cbbd163fd5b11f298460660d2
avyukd <avyukd@gmail.com>
2022-07-12 14:59:32 -0400

    Time: 84 ms (51.61%), Space: 17.5 MB (93.54%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -5,13 +5,52 @@
 #         self.next = None
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = set()
-        tmp = head
-        while tmp is not None:
-            if id(tmp) in visited:
+    def hasCycle(self, head):
+        if head is None:
+            return False
+        slow, fast = head, head.next
+        while fast is not None:
+            if slow == fast:
                 return True
-            else:
-                visited.add(id(tmp))
-            tmp = tmp.next
+            slow = slow.next
+            fast = fast.next
+            if fast is None:
+                return False
+            fast = fast.next
         return False
+    
+    
+    
+    
+    
+    
+    
+#     # O(1) solution
+#     def hasCycle(self, head) -> bool:
+#         slow, fast = head, head
+        
+#         if slow.next is None:
+#             return False
+        
+#         while slow is not None and fast is not None:
+            
+#             slow = slow.next
+#             fast = fast.next
+#             if fast is not None:
+#                 fast = fast.next
+            
+#             if id(slow) == id(fast):
+#                 return True
+
+        
+#         return False
+# #     def hasCycle(self, head: Optional[ListNode]) -> bool:
+# #         visited = set()
+# #         tmp = head
+# #         while tmp is not None:
+# #             if id(tmp) in visited:
+# #                 return True
+# #             else:
+# #                 visited.add(id(tmp))
+# #             tmp = tmp.next
+# #         return False
