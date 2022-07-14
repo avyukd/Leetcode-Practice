6cc06edb08b2cd352974e670d072fcfebcc772bf
avyukd <avyukd@gmail.com>
2022-07-14 17:46:03 -0400

    Time: 56 ms (46.55%), Space: 14.3 MB (30.03%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,18 +1,43 @@
 class Solution:
-    # alternatively: could do transpose + reverse solution, worth trying out later
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix: List[List[int]]) -> None:    
+        n = len(matrix)
         def transpose():
-            for i in range(len(matrix)):
-                for j in range(i+1, len(matrix)):
+            for i in range(n):
+                for j in range(i, n):
                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-            
-        def reflect():
-            for i in range(len(matrix)):
-                for j in range(len(matrix) // 2):
-                    matrix[i][j], matrix[i][len(matrix) - 1 - j] = matrix[i][len(matrix) - 1 - j], matrix[i][j]
         
+        def reflect():
+            for i in range(n): 
+                for j in range(n // 2):
+                    print(i, j, n - j - 1)
+                    matrix[i][j], matrix[i][n - j - 1]  = matrix[i][n - j - 1], matrix[i][j] 
         transpose()
         reflect()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     # alternatively: could do transpose + reverse solution, worth trying out later
+#     def rotate(self, matrix: List[List[int]]) -> None:
+#         def transpose():
+#             for i in range(len(matrix)):
+#                 for j in range(i+1, len(matrix)):
+#                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+#         def reflect():
+#             for i in range(len(matrix)):
+#                 for j in range(len(matrix) // 2):
+#                     matrix[i][j], matrix[i][len(matrix) - 1 - j] = matrix[i][len(matrix) - 1 - j], matrix[i][j]
+        
+#         transpose()
+#        reflect()
     # swap groups of 4 cells
 #     def rotate(self, matrix: List[List[int]]) -> None:
 #         """
