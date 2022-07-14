cfe7955695622643031584ba1ff89a932a77d94b
avyukd <avyukd@gmail.com>
2022-07-14 18:15:07 -0400

    Time: 524 ms (14.57%), Space: 14.5 MB (13.34%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,22 +1,53 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
-        maxSubLen = 0
-        seen = set()
-        i, j = 0, 0
+        i, j = 0, 0 
+        seen = defaultdict(int)
+        maxWindow = 0
         while j < len(s):
             if s[j] in seen:
-                print(j, i)
-                maxSubLen = max(maxSubLen, j - i)
-                while s[i] != s[j]:
-                    seen.remove(s[i])
-                    i += 1
-                i += 1
-            else:
-                seen.add(s[j])
+                i = seen[s[j]] + 1
+                for key in list(seen.keys()):
+                    if seen[key] < i:
+                        del seen[key]
+            print(i, j)
+            seen[s[j]] = j
+            maxWindow = max(maxWindow, j - i + 1)
             j += 1
+        return maxWindow
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if len(s) <= 1:
+#             return len(s)
+#         maxSubLen = 0
+#         seen = set()
+#         i, j = 0, 0
+#         while j < len(s):
+#             if s[j] in seen:
+#                 print(j, i)
+#                 maxSubLen = max(maxSubLen, j - i)
+#                 while s[i] != s[j]:
+#                     seen.remove(s[i])
+#                     i += 1
+#                 i += 1
+#             else:
+#                 seen.add(s[j])
+#             j += 1
         
-        maxSubLen = max(maxSubLen, j - i)
+#         maxSubLen = max(maxSubLen, j - i)
         
-        return maxSubLen
+#         return maxSubLen
