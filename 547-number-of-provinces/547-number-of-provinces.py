306dfca5c10b2d8c529ae103f1708be44377c897
avyukd <avyukd@gmail.com>
2022-07-16 13:13:00 -0400

    Time: 469 ms (8.84%), Space: 14.1 MB (96.17%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,54 +1,54 @@
 class Solution:
-    # dfs approach
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        numProvinces = 0
-        
-        visited = set()
-        
-        def dfs(i_in):
-            stack = deque([i_in])
-            while stack:
-                i = stack.pop()
-                if i not in visited:
-                    visited.add(i)
-                    for j in range(len(isConnected)):
-                        if isConnected[i][j] == 1:
-                            stack.append(j)
-        
-        for i in range(len(isConnected)):
-            if i not in visited:
-                dfs(i)
-                numProvinces += 1
-        
-        return numProvinces
-        
-#     class UnionFind:
-#         def __init__(self, size):
-#             self.root = [i for i in range(size)]
-        
-#         def find(self, x):
-#             if x == self.root[x]:
-#                 return x
-#             self.root[x] = self.find(self.root[x])
-#             return self.root[x]
-        
-#         def unite(self, x, y):
-#             rootX, rootY = self.find(x), self.find(y)
-#             if rootX != rootY:
-#                 self.root[rootY] = rootX
-        
-#         def numComponents(self):
-#             return len(set(self.root))
-        
+#     # dfs approach
 #     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-#         n = len(isConnected)
-#         uf = self.UnionFind(n)
-#         for i in range(n):
-#             for j in range(n):
-#                 if i < j and isConnected[i][j] == 1:
-#                     uf.unite(i, j)
+#         numProvinces = 0
         
-#         for i in range(n):
-#             uf.find(i)
+#         visited = set()
+        
+#         def dfs(i_in):
+#             stack = deque([i_in])
+#             while stack:
+#                 i = stack.pop()
+#                 if i not in visited:
+#                     visited.add(i)
+#                     for j in range(len(isConnected)):
+#                         if isConnected[i][j] == 1:
+#                             stack.append(j)
+        
+#         for i in range(len(isConnected)):
+#             if i not in visited:
+#                 dfs(i)
+#                 numProvinces += 1
+        
+#         return numProvinces
+        
+    class UnionFind:
+        def __init__(self, size):
+            self.root = [i for i in range(size)]
+        
+        def find(self, x):
+            if x == self.root[x]:
+                return x
+            self.root[x] = self.find(self.root[x])
+            return self.root[x]
+        
+        def unite(self, x, y):
+            rootX, rootY = self.find(x), self.find(y)
+            if rootX != rootY:
+                self.root[rootY] = rootX
+        
+        def numComponents(self):
+            return len(set(self.root))
+        
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = self.UnionFind(n)
+        for i in range(n):
+            for j in range(n):
+                if i < j and isConnected[i][j] == 1:
+                    uf.unite(i, j)
+        
+        for i in range(n):
+            uf.find(i)
             
-#         return uf.numComponents()
+        return uf.numComponents()
