344e26d449e079b1a0df0a77e31c0e1328f0537f
avyukd <avyukd@gmail.com>
2022-07-17 11:13:40 -0400

    Time: 687 ms (43.76%), Space: 15.1 MB (67.96%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,36 +1,75 @@
 class Solution:
-    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        
+    def longestIncreasingPath(self, matrix):
         m, n = len(matrix), len(matrix[0])
+        pathLens = [[0] * n for _ in range(m)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        for i in range(m):
+            for j in range(n):
+                maxLocal = True
+                for d in directions:
+                    if 0 <= i + d[0] < m and 0 <= j + d[1] < n: 
+                        if matrix[i + d[0]][j + d[1]] > matrix[i][j]:
+                            maxLocal = False
+                            break
+                if maxLocal:
+                    pathLens[i][j] = 1
+        visited = set()
+        def dfs(i, j):
+            if pathLens[i][j] != 0:
+                return pathLens[i][j]
+            maxPathLen = 0
+            for d in directions:
+                if 0 <= i + d[0] < m and 0 <= j + d[1] < n:
+                    if (i + d[0], j + d[1]) not in visited:
+                        if matrix[i + d[0]][j + d[1]] > matrix[i][j]:
+                            visited.add((i + d[0], j + d[1]))
+                            maxPathLen = max(maxPathLen, 1 + dfs(i+d[0], j+d[1]))
+                            visited.remove((i + d[0], j + d[1]))
+            pathLens[i][j] = maxPathLen
+            return pathLens[i][j]
         
-        memo = {}
-        def findLis(row, col, old):
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j)
+        
+        maxPath = 0
+        for i in range(m):
+            for j in range(n):
+                maxPath = max(maxPath, pathLens[i][j])
+        return maxPath
+# class Solution:
+#     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        
+#         m, n = len(matrix), len(matrix[0])
+        
+#         memo = {}
+#         def findLis(row, col, old):
             
-            if row < 0 or col < 0 or row >= m or col >= n:
-                return 0
+#             if row < 0 or col < 0 or row >= m or col >= n:
+#                 return 0
             
-            curr = matrix[row][col]
+#             curr = matrix[row][col]
             
-            if curr <= old:
-                return 0
+#             if curr <= old:
+#                 return 0
             
-            if (row, col) in memo:
-                return memo[(row, col)]
+#             if (row, col) in memo:
+#                 return memo[(row, col)]
             
-            up = findLis(row - 1, col, curr)
-            down = findLis(row + 1, col, curr)
-            left = findLis(row, col - 1, curr)
-            right = findLis(row, col + 1, curr)
-            l = 1 + max(up, down, left, right)
-            memo[(row, col)] = l
-            return l
+#             up = findLis(row - 1, col, curr)
+#             down = findLis(row + 1, col, curr)
+#             left = findLis(row, col - 1, curr)
+#             right = findLis(row, col + 1, curr)
+#             l = 1 + max(up, down, left, right)
+#             memo[(row, col)] = l
+#             return l
 
-        maxLen = 0
-        for row in range(m):
-            for col in range(n):
-                l = findLis(row, col, -1)
-                maxLen = max(l, maxLen)
+#         maxLen = 0
+#         for row in range(m):
+#             for col in range(n):
+#                 l = findLis(row, col, -1)
+#                 maxLen = max(l, maxLen)
         
-        return maxLen
+#         return maxLen
          
         
