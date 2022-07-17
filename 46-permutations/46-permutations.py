c61d25b032d9aabd726ee29db9133322edbbf7e4
avyukd <avyukd@gmail.com>
2022-07-17 12:58:26 -0400

    Time: 63 ms (47.52%), Space: 14.1 MB (21.84%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,29 +1,29 @@
 class Solution:
-    def permute(self, nums):
-        # swapping algo
+    # def permute(self, nums):
+    #     # swapping algo
+    #     res = []
+    #     perm = []
+    #     def backtrack(first):
+    #         if first == len(nums) - 1:
+    #             res.append(nums[:])
+    #         for i in range(first, len(nums)):
+    #             nums[first], nums[i] = nums[i], nums[first]
+    #             backtrack(first + 1)
+    #             nums[first], nums[i] = nums[i], nums[first]
+    #     backtrack(0)
+    #    return res
+    def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         perm = []
-        def backtrack(first):
-            if first == len(nums) - 1:
-                res.append(nums[:])
-            for i in range(first, len(nums)):
-                nums[first], nums[i] = nums[i], nums[first]
-                backtrack(first + 1)
-                nums[first], nums[i] = nums[i], nums[first]
-        backtrack(0)
-        return res
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         res = []
-#         perm = []
-#         def backtrack():
-#             if len(perm) == len(nums):
-#                 res.append(perm.copy())
-#                 return
+        def backtrack():
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
             
-#             for i in range(len(nums)):
-#                 if nums[i] not in perm:
-#                     perm.append(nums[i])
-#                     backtrack()
-#                     perm.pop()
-#         backtrack()
-#         return res
+            for i in range(len(nums)):
+                if nums[i] not in perm:
+                    perm.append(nums[i])
+                    backtrack()
+                    perm.pop()
+        backtrack()
+        return res
