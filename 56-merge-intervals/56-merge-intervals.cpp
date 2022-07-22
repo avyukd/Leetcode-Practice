d5a5e496b24627c334d1da0d44f2dc6525736f79
avyukd <avyukd@gmail.com>
2022-07-22 11:27:42 -0400

    Time: 73 ms (31.63%), Space: 19.1 MB (66.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,8 +3,7 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         
-        vector<vector<int>> merged;
-        merged.push_back(intervals[0]);
+        vector<vector<int>> merged = {intervals[0]};
         for(auto &interval: intervals){
             if(merged[merged.size() - 1][1] >= interval[0]){
                 merged[merged.size() - 1][1] = max(merged[merged.size() - 1][1], interval[1]);
