23bce41c484cbc4191a3741337f16581c7743a44
avyukd <avyukd@gmail.com>
2022-07-23 15:32:21 -0400

    Time: 754 ms (93.66%), Space: 28.6 MB (9.82%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,14 +1,13 @@
-class Solution:
+class Solution:    
     def maxSubArray(self, nums):
+        maxSum = nums[0]
         maxVal = nums[0]
-        currVal = 0
-        for num in nums:
-            if currVal < 0:
-                currVal = num
-            else:
-                currVal += num
-            maxVal = max(maxVal, currVal)
-        return maxVal
+        for i in range(1, len(nums)):
+            if maxVal < 0:
+                maxVal = 0
+            maxVal += nums[i]
+            maxSum = max(maxVal, maxSum)
+        return maxSum
     
     
     
@@ -22,13 +21,23 @@ class Solution:
     
     
     
+#     def maxSubArray(self, nums):
+#         maxVal = nums[0]
+#         currVal = 0
+#         for num in nums:
+#             if currVal < 0:
+#                 currVal = num
+#             else:
+#                 currVal += num
+#             maxVal = max(maxVal, currVal)
+#         return maxVal
     
-    # def maxSubArray(self, nums: List[int]) -> int:
-    #     current_subarray = 0
-    #     max_subarray = 0
-    #     for num in nums:
-    #         current_subarray = max(0, current_subarray + num)
-    #         max_subarray = max(current_subarray, max_subarray)
-    #     if max_subarray == 0:
-    #         return max(nums)
-    #     return max_subarray
+#     # def maxSubArray(self, nums: List[int]) -> int:
+#     #     current_subarray = 0
+#     #     max_subarray = 0
+#     #     for num in nums:
+#     #         current_subarray = max(0, current_subarray + num)
+#     #         max_subarray = max(current_subarray, max_subarray)
+#     #     if max_subarray == 0:
+#     #         return max(nums)
+#     #     return max_subarray
