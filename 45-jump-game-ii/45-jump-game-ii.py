d83e12e9e12deeab129e52006dd3bf1896a566ba
avyukd <avyukd@gmail.com>
2022-07-23 17:44:50 -0400

    Time: 18126 ms (5.00%), Space: 28.1 MB (5.03%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,12 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        @cache
+        memo = [-1] * len(nums)
         def jumpHelper(i):
             if i >= len(nums) - 1:
                 return 0
+            if memo[i] != -1:
+                return memo[i]
             maxJump = nums[i]
             minJumps = len(nums)
             for jumpLen in range(1, maxJump + 1):
                 minJumps = min(minJumps, 1 + jumpHelper(i + jumpLen))
-            return minJumps            
+            memo[i] = minJumps
+            return memo[i]
         return jumpHelper(0)
+    
+        
