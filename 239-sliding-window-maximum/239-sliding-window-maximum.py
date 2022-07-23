0bf496bd1a8e44162bd23c56521db232091e9b76
avyukd <avyukd@gmail.com>
2022-07-23 12:14:26 -0400

    Time: 2678 ms (53.92%), Space: 29.8 MB (77.08%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,37 +1,71 @@
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    def maxSlidingWindow(self, nums, k):
+        maxSliding = []
+        
         monoq = deque([])
-        results = []
-        # get the first window
-        for i in range(k):
-            if len(monoq) == 0:
+        for i in range(k):    
+            if not monoq:
                 monoq.append(i)
             else:
-                if nums[i] >= nums[monoq[0]]:
-                    monoq = deque([i])
+                if nums[i] > nums[monoq[0]]:
+                    monoq = deque([i]) 
                 else:
-                    if nums[i] < nums[monoq[-1]]:
-                        monoq.append(i)
-                    else:
-                        while nums[i] >= nums[monoq[-1]]:
-                            monoq.pop()
-                        monoq.append(i)
-        results.append(nums[monoq[0]])
-        for i in range(k, len(nums)):
-            while monoq and monoq[0] <= i - k:
+                    while monoq and nums[i] > nums[monoq[-1]]:
+                        monoq.pop()
+                    monoq.append(i)    
+        
+        maxSliding.append(nums[monoq[0]])
+        
+        for start in range(1, len(nums) - k + 1):
+            while monoq and monoq[0] < start:    
                 monoq.popleft()
-            if len(monoq) == 0:
-                monoq.append(i)
+            if not monoq:
+                monoq.append(start + k - 1)
+            elif nums[start + k - 1] > nums[monoq[0]]:
+                monoq = deque([start + k - 1])
             else:
-                if nums[i] >= nums[monoq[0]]:
-                    monoq = deque([i])
-                else:
-                    if nums[i] < nums[monoq[-1]]:
-                        monoq.append(i)
-                    else:
-                        while nums[i] >= nums[monoq[-1]]:
-                            monoq.pop()
-                        monoq.append(i)
-            results.append(nums[monoq[0]])
-        return results
+                while monoq and nums[start + k - 1] > nums[monoq[-1]]:
+                    monoq.pop()
+                monoq.append(start + k - 1)  
+            maxSliding.append(nums[monoq[0]])
+        return maxSliding
+            
+            
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         monoq = deque([])
+#         results = []
+#         # get the first window
+#         for i in range(k):
+#             if len(monoq) == 0:
+#                 monoq.append(i)
+#             else:
+#                 if nums[i] >= nums[monoq[0]]:
+#                     monoq = deque([i])
+#                 else:
+#                     if nums[i] < nums[monoq[-1]]:
+#                         monoq.append(i)
+#                     else:
+#                         while nums[i] >= nums[monoq[-1]]:
+#                             monoq.pop()
+#                         monoq.append(i)
+#         results.append(nums[monoq[0]])
+#         for i in range(0, len(nums)):
+#             if i >= k:
+#                 while monoq and monoq[0] <= i - k:
+#                     monoq.popleft()
+#             if len(monoq) == 0:
+#                 monoq.append(i)
+#             else:
+#                 if nums[i] >= nums[monoq[0]]:
+#                     monoq = deque([i])
+#                 else:
+#                     if nums[i] < nums[monoq[-1]]:
+#                         monoq.append(i)
+#                     else:
+#                         while nums[i] >= nums[monoq[-1]]:
+#                             monoq.pop()
+#                         monoq.append(i)
+#             if i >= k:
+#                 results.append(nums[monoq[0]])
+#         return results
             
