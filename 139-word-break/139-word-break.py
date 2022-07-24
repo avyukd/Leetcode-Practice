f5c9fb86c3b074ec85b56bada50922bc1543d5f2
avyukd <avyukd@gmail.com>
2022-07-24 18:39:23 -0400

    Time: 43 ms (87.38%), Space: 14.2 MB (14.33%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,18 +1,20 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        @cache
-        def backtrack(word, i):
+        memo = {}
+        def backtrack(word):
+            if word in memo:
+                return memo[word]
+            
             if len(word) == 0:
                 return True
                 
             for prefix in wordDict:
                 if prefix == word[:len(prefix)]:
-                    if len(prefix) <= len(word):
-                        curr = backtrack(word[len(prefix):], len(prefix))
-                        if curr:
-                            return True
+                    if len(prefix) <= len(word) and backtrack(word[len(prefix):]):
+                        return True
             
+            memo[word] = False
             return False
     
-        return backtrack(s, 0)
+        return backtrack(s)
