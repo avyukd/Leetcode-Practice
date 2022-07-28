7b2b60c6446525028ab5d6367d7d66b561749395
avyukd <avyukd@gmail.com>
2022-07-28 18:30:56 -0400

    Time: 875 ms (5.01%), Space: 14.6 MB (31.18%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -1,4 +1,30 @@
 class Solution:
+    class UnionFind:
+        def __init__(self, size):
+            self.reps = [i for i in range(0, size + 1)]
+            self.size = size
+        
+        def find(self, n):
+            if self.reps[n] == n:
+                return n
+            self.reps[n] = self.find(self.reps[n])
+            return self.reps[n]
+        
+        def unite(self, n1, n2):
+            root1, root2 = self.find(n1), self.find(n2)
+            self.reps[root2] = root1
+    
+    def findRedundantConnection(self, edges):
+        maxNode = 1
+        for edge in edges:
+            maxNode = max([edge[0], edge[1], maxNode])
+        uf = UnionFind(maxNode)
+        for edge in edges:
+            if uf.find(edge[0]) == uf.find(edge[1]):
+                return edge
+            uf.unite(edge[0], edge[1])
+        return []
+    
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         adj = defaultdict(set)
         for edge in edges:
