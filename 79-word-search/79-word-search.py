cedb872795f3fa280e700d025a245d5b1ccaf660
avyukd <avyukd@gmail.com>
2022-08-10 18:34:47 -0400

    Time: 8707 ms (27.61%), Space: 14 MB (51.13%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,31 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
         
-        def backtrack(row, col, suffix):
-            if suffix == "":
-                return True
-            
-            if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]):
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        def dfs(i, j, depth):
+            if (i, j) in visited:
                 return False
-            
-            if board[row][col] != suffix[0] or board[row][col] == "@":
-                return False
-            
-            res = False
-            board[row][col] = "@"
-            
-            res = res or backtrack(row + 1, col, suffix[1:])
-            res = res or backtrack(row - 1, col, suffix[1:])
-            res = res or backtrack(row, col + 1, suffix[1:])
-            res = res or backtrack(row, col - 1, suffix[1:])
-            
-            board[row][col] = suffix[0]
-            
-            return res
+                        
+            if board[i][j] == word[depth]:
+                
+                if depth == len(word) - 1:
+                    return True
+                
+                for dx, dy in dirs:
+                    ni, nj = i + dx, j + dy
+                    if 0 <= ni < m and 0 <= nj < n:
+                        visited.add((i, j))
+                        if dfs(ni, nj, depth + 1):
+                            return True
+                        visited.remove((i, j))
+            return False
         
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                if backtrack(row, col, word):
+        for i in range(m):
+            for j in range(n):
+                visited = set()
+                if dfs(i, j, 0):
                     return True
         
         return False
