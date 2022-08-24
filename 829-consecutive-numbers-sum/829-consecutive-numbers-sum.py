57f5bd3fc8285473035a0647b16e64da0ad9ff1b
avyukd <avyukd@gmail.com>
2022-08-24 18:11:20 -0400

    Time: 613 ms (5.52%), Space: 13.8 MB (54.38%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
@@ -1,32 +1,13 @@
 class Solution:
     def consecutiveNumbersSum(self, n: int) -> int:
-        if n == 1:
-            return 1
-        ways = 0
-        for k in range(1, ceil((2 * n + 0.25)**0.5 - 0.5) + 1):
-            rem = n - ((k * (k + 1)) // 2)
-            if rem >= 0 and rem % k == 0:
-                ways += 1
-                if rem == 0:
-                    break
-        return ways
-    
-#         ways = 0
+        # n = kx + (k * (k + 1)) / 2, for all k
         
-#         memo = {}
-#         def recurse(num, lastSeqNum):
-#             if num == lastSeqNum:
-#                 return 1
-#             elif lastSeqNum <= 1:
-#                 return 0
-#             elif num < lastSeqNum:
-#                 return 0
-#             else:
-#                 if (num - lastSeqNum, lastSeqNum - 1) not in memo:
-#                     memo[(num - lastSeqNum, lastSeqNum - 1)] = recurse(num - lastSeqNum, lastSeqNum - 1)
-#                 return memo[(num - lastSeqNum, lastSeqNum - 1)]
+        ways, k = 0, 1
+        while (k * (k + 1)) / 2 <= n:
             
-#         for i in range(1, n):
-#             ways += recurse(n, i)
+            if ( n - (k * (k + 1)) / 2 ) % k == 0:
+                ways += 1
+            
+            k += 1
         
-#         return ways + 1
+        return ways
